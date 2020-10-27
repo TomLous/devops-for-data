d7338de5b8506f0615e12ab294134c96cb8b2e28
Tom Lous <tomlous@gmail.com>
2020-10-27 14:00:54 +0100

    dev deploy flow

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+import shlex
+import sys
+from subprocess import check_output
+
 from setuptools import setup
 
+GIT_HEAD_REV = check_output(shlex.split('git rev-parse --short HEAD')).decode(sys.stdout.encoding).strip()
+STABLE_VERSION = True
+
+try:
+    check_output(shlex.split('git describe --exact-match --tags HEAD')).decode(sys.stdout.encoding).strip()
+except:
+    STABLE_VERSION = False
+
+if STABLE_VERSION:
+    tag_build = ""
+else:
+    tag_build = "_" + GIT_HEAD_REV
+
 # All config is in setup.cfg
-setup()
+setup(
+    options=dict(egg_info=dict(tag_build=tag_build))
+)
